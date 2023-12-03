f0df7337b27a372332e8bbc4c2005092761ca11f
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2023-12-02 21:39:54 -0600

    added firebase and is a working version

--- public/index.js
+++ public/index.js
@@ -1,10 +1,15 @@
 const inputObj = document.getElementById("input")
 const inputDisplayObj = document.getElementById("input-display")
+const restartButton = document.getElementById("restart-button")
+const timerDisplay = document.getElementById("timer")
 
 const possibleWords = ["meowww", "purrr", "meeeeow", "meoooow"]
 const maxCharLength = 50
 
 let targetString
+let timeStart
+let gameStarted
+let gameEnded
 
 function updateTargetString()
 {
@@ -21,19 +26,40 @@ function updateTargetString()
     targetString = newString.substring(1);
 }
 
+function gameStart()
+{
+    gameStarted = true
+    gameEnded = false
+    timeStart = Date.now()
+}
+
+function endGame()
+{
+    gameStarted = false
+    gameEnded = true
+
+    let elapsedTime = (Date.now()-timeStart)/1000
+    elapsedTime = Math.floor(elapsedTime*10)/10
+
+    let meowsPerMin = targetString.split(" ").length / elapsedTime * 60
+    meowsPerMin = Math.floor(meowsPerMin*10)/10
+
+    timerDisplay.innerHTML = `${elapsedTime}s : Meows per min ${meowsPerMin}`
+}
+
 function correctChar(char)
 {
-    return `<span style="color: green">${char}</span>`
+    return `<span class="correct-char">${char}</span>`
 }
 
 function incorrectChar(char)
 {
-    return `<span style="color: red">${char}</span>`
+    return `<span class="incorrect-char">${char}</span>`
 }
 
 function missingChar(char)
 {
-    return `<span style="color: grey">${char}</span>`
+    return `<span class="missing-char">${char}</span>`
 }
 
 function updateDisplay(inputValue)
@@ -49,6 +75,8 @@ function updateDisplay(inputValue)
                 displayHTML += incorrectChar(inputValue.charAt(i))
         } else if (i < targetString.length && i >= inputValue.length) {
             displayHTML += missingChar(targetString.charAt(i))
+        } else {
+            displayHTML += incorrectChar(inputValue.charAt(i))
         }
     }
     inputDisplayObj.innerHTML = displayHTML
@@ -58,7 +86,35 @@ updateTargetString()
 console.log(targetString)
 
 inputObj.oninput = (e) => {
+    if (!gameStarted && !gameEnded)
+        gameStart();
+
+    inputObj.value = inputObj.value.replace("\n", " ")
     updateDisplay(inputObj.value)
+
+    if (inputObj.value == targetString)
+        endGame()
 }
 
 updateDisplay("")
+
+function timer_update()
+{
+    if (gameStarted)
+    {
+        let elapsedTime = (Date.now()-timeStart)/1000
+        elapsedTime = Math.floor(elapsedTime*10)/10
+        timerDisplay.innerHTML = ""+elapsedTime+'s'
+    }
+    requestAnimationFrame(timer_update)
+}
+
+restartButton.onclick = () => {
+    updateTargetString()
+    gameStarted = false
+    gameEnded = false
+    inputObj.value = ""
+    updateDisplay("")
+}
+
+timer_update()
